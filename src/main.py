79993c8d9d262aaacb8cf2cc9e1d640865b687e4
Ricard Mallafré López <rikitzzz@gmail.com>
2020-06-19 18:36:37 +0200

    removed hello, world; changed variables names

--- src/main.py
+++ src/main.py
@@ -1,17 +1,15 @@
 from pprint import pprint
 import json
 
-print('Hello, world!')
-
 s = 'pusvwxvvwvwtwwwwxvwuxwwwxwxtwwwvwvxxwwxsuwwtxtwuvwxxsvxvxttwwrxwxwvvxwxttwxsxuuwtuwtuxuuwutwutxuvwtuwttxuuwttxuuw'
 b = bytearray(s.encode())
-result = list(b)
+samples = list(b)
 
 run = {
     'runid': 0,
     'cdIn': '00 11 22 33 44 55 66 77',
     'cdOut': '77 66 55 44 33 22 11',
-    'samples': result
+    'samples': samples
 }
 
 pprint(run)
